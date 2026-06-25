23ac218452504c5cc127db2168fe881c89fada54
ujjwal36512 <ujjwalacharya365@gmail.com>
2026-06-25 11:12:51 +0545

    updated forms.py

--- recipes/forms.py
+++ recipes/forms.py
@@ -12,3 +12,17 @@ class RecipeForm(forms.ModelForm):
             'chefs_note',
             'cover_image',
         ]
+        
+class RecipeSearchForm(forms.Form):
+    query = forms.CharField(max_length=100, required=False,
+         widget= forms.TextInput(attrs ={
+                    'placeholder':'Search recipes by title or ingredient..',
+                    'class':'search-input'
+               }))
+    def clean_query(self):
+          query = self.cleaned_data.get('query')
+          if query:
+               query - query.strip()
+               if len(query)<2:
+                    raise forms.ValidationError('search term must be at least 2 characters.')
+          return query
